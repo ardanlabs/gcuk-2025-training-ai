88f541c4aa1178dfe4d2154d24c47039465a54ee
Florin Patan <florin@Florins-MacBook-Pro.local>
2025-08-13 13:46:46 +0100

    Example09-step1-1

---
+++ cmd/examples/example09/step1/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+const (
+	url       = "http://localhost:11434"
+	model     = "qwen2.5vl:latest"
+	imagePath = "cmd/samples/gallery/roseimg.png"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	ctx := context.Background()
+
+	// -------------------------------------------------------------------------
+
+	llm, err := ollama.New(
+		ollama.WithModel(model),
+		ollama.WithServerURL(url),
+	)
+	if err != nil {
+		return fmt.Errorf("ollama: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+
+	data, mimeType, err := processImage(imagePath)
+	if err != nil {
+		return fmt.Errorf("process image: %w", err)
+	}
+
+	// -------------------------------------------------------------------------
+
+	fmt.Print("\nGenerating image description:\n\n")
+
+	prompt := `Describe the image. Be concise and accurate. Do not be overly
+	verbose or stylistic. Make sure all the elements in the image are
+	enumerated and described. Do not include any additional details. Keep
+	the description under 200 words. At the end of the description, create
+	a list of tags with the names of all the elements in the image. Do not
+	output anything past this list.
+	Encode the list as valid JSON, as in this example:
+	[
+		"tag1",
+		"tag2",
+		"tag3",
+		...
+	]
+	Make sure the JSON is valid, doesn't have any extra spaces, and is
+	properly formatted.`
+
+	messages := []llms.MessageContent{
+		{
+			Role: llms.ChatMessageTypeHuman,
+			Parts: []llms.ContentPart{
+				llms.BinaryContent{
+					MIMEType: mimeType,
+					Data:     data,
+				},
+				llms.TextContent{
+					Text: prompt,
+				},
+			},
+		},
+	}
+
+	cr, err := llm.GenerateContent(
+		ctx,
+		messages,
+		llms.WithMaxTokens(500),
+		llms.WithTemperature(1.0),
+	)
+	if err != nil {
+		return fmt.Errorf("generate content: %w", err)
+	}
+
+	fmt.Print(cr.Choices[0].Content)
+	fmt.Print("\n\n")
+
+	return nil
+}
+
+func processImage(fileName string) ([]byte, string, error) {
+	data, err := readImage(fileName)
+	if err != nil {
+		return nil, "", fmt.Errorf("read image: %w", err)
+	}
+
+	var mimeType string
+	switch filepath.Ext(fileName) {
+	case ".jpg", ".jpeg":
+		mimeType = "image/jpg"
+	case ".png":
+		mimeType = "image/png"
+	default:
+		return nil, "", fmt.Errorf("unsupported file type: %s", filepath.Ext(fileName))
+	}
+
+	return data, mimeType, nil
+}
+
+func readImage(fileName string) ([]byte, error) {
+	f, err := os.OpenFile(fileName, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, fmt.Errorf("open file: %w", err)
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("read file: %w", err)
+	}
+
+	return data, nil
+}
