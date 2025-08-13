759528901273b8f7d9a3ae494db0c9807fc66bbb
Florin Patan <florin@Florins-MacBook-Pro.local>
2025-08-13 09:52:45 +0100

    Example 02-1

---
+++ cmd/examples/example02/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ardanlabs/ai-training/foundation/vector"
+	"github.com/tmc/langchaingo/llms/ollama"
+)
+
+const (
+	url   = "http://localhost:11434"
+	model = "bge-m3:latest"
+)
+
+// =============================================================================
+
+type data struct {
+	Name      string
+	Text      string
+	Embedding []float32 // The vector where the data is embedded in space.
+}
+
+// Vector can convert the specified data into a vector.
+func (d data) Vector() []float32 {
+	return d.Embedding
+}
+
+func main() {
+
+	llm, err := ollama.New(
+		ollama.WithModel(model),
+		ollama.WithServerURL(url),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Old way of representing this data with our own vector data points.
+	// dataPoints := []vector.Data{
+	// 	data{Name: "Horse   ", Authority: 0.0, Animal: 1.0, Human: 0.0, Rich: 0.0, Gender: +1.0},
+	// 	data{Name: "Man     ", Authority: 0.0, Animal: 0.0, Human: 1.0, Rich: 0.0, Gender: -1.0},
+	// 	data{Name: "Woman   ", Authority: 0.0, Animal: 0.0, Human: 1.0, Rich: 0.0, Gender: +1.0},
+	// 	data{Name: "King    ", Authority: 1.0, Animal: 0.0, Human: 1.0, Rich: 1.0, Gender: -1.0},
+	// 	data{Name: "Queen   ", Authority: 1.0, Animal: 0.0, Human: 1.0, Rich: 1.0, Gender: +1.0},
+	// }
+
+	// Apply the feature vectors to the hand crafted data points.
+	// This time you need to use words since we are using a word based model.
+	dataPoints := []vector.Data{
+		data{Name: "Horse   ", Text: "Animal, Female"},
+		data{Name: "Man     ", Text: "Human,  Male,   Pants, Poor, Worker"},
+		data{Name: "Woman   ", Text: "Human,  Female, Dress, Poor, Worker"},
+		data{Name: "King    ", Text: "Human,  Male,   Pants, Rich, Ruler"},
+		data{Name: "Queen   ", Text: "Human,  Female, Dress, Rich, Ruler"},
+	}
+
+	fmt.Print("\n")
+
+	// Iterate over each data point and use the LLM to generate the vector
+	// embedding related to the model.
+	for i, dp := range dataPoints {
+		dataPoint := dp.(data)
+
+		vectors, err := llm.CreateEmbedding(context.Background(), []string{dataPoint.Text})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dataPoint.Embedding = vectors[0]
+		dataPoints[i] = dataPoint
+
+		fmt.Printf("Vector: Name(%s) len(%d) %v...%v\n", dataPoint.Name, len(vectors[0]), vectors[0][0:2], vectors[0][len(vectors[0])-2:])
+	}
+
+	// -------------------------------------------------------------------------
+
+	// Compare each data point to every other by performing a cosine
+	// similarity comparison using the vector embedding from the LLM.
+	for _, target := range dataPoints {
+		results := vector.Similarity(target, dataPoints...)
+
+		for _, result := range results {
+			fmt.Printf("%s -> %s: %.2f%% similar\n",
+				result.Target.(data).Name,
+				result.DataPoint.(data).Name,
+				result.Percentage)
+		}
+		fmt.Print("\n")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dlclark/regexp2 v1.11.5
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/tmc/langchaingo v0.1.13
 	go.mongodb.org/mongo-driver v1.17.4
 	golang.org/x/text v0.28.0
 )
@@ -14,12 +15,13 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
